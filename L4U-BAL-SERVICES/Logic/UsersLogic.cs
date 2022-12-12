de3686ca6cb31f0c5ad4a1f8b51764b1ae6b068a
Sérgio <Srgio@DESKTOP-2V1QCKI>
2022-12-12 23:32:59 +0000

    Add project files.

---
+++ L4U-BAL-SERVICES/Logic/UsersLogic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace L4U_BAL_SERVICES.Logic
+{
+    internal class UsersLogic
+    {
+    }
+}
---
+++ L4U-BOL-MODEL/Models/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace L4U_BOL_MODEL.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+
+        [JsonIgnore]
+        public string Password { get; set; }
+
+        public DateTime DateOfCreation { get; set; }
+        public string Token { get; set; } = string.Empty;
+
+        public User()
+        { }
+
+        public User(object obj)
+        { }
+
+        public User(DataRow dr)
+        {
+            this.Id = dr["id"].ToString();
+            this.Name = dr["name"].ToString();
+            this.Email = dr["email"].ToString();
+            this.Username = dr["username"].ToString();
+            this.Password = dr["password"].ToString();
+            this.DateOfCreation = DateTime.Parse(dr["dateOfCreation"].ToString());
+        }
+    }
+}
