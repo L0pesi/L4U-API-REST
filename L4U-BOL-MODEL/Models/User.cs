27e441809ce3fa742ad5044331ad3f2e5ab4aab0
diogorocha1999 <diogocorreia1999@gmail.com>
2022-12-13 21:25:58 +0000

    branch test 2

--- L4U-BOL-MODEL/Models/User.cs
+++ L4U-BOL-MODEL/Models/User.cs
@@ -28,7 +28,7 @@ namespace L4U_BOL_MODEL.Models
 
         public User(object obj)
         { }
-
+        asdasasdasd
         public User(DataRow dr)
         {
             this.Id = dr["id"].ToString();
