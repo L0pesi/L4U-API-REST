84dbe9076e42ab61932d21bf827e5811360e282d
sergio0140 <a18858@alunos.ipca.pt>
2022-12-13 16:38:09 +0000

    teste commit

---
+++ L4U-BOL-MODEL/Models/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L4U_BOL_MODEL.Models
+{
+    internal class Class1
+    {
+    }
+}
