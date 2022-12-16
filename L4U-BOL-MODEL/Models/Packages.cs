37d1dc28dbced23dff42c15334fd5b4f5e052215
L0pesi <sergiomaciel21@gmail.com>
2022-12-16 17:35:43 +0000

    Feito pelo Yuno!

---
+++ L4U-BOL-MODEL/Models/Packages.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L4U_BOL_MODEL.Models
+{
+    internal class Packages
+    {
+        public string Id { get; set; }
+        public string Adress { get; set; }
+        public string PostalCode { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public string Name { get; set; }
+        public int IdentifierCode { get; set; } //alterei para public int
+
+        public Packages() { }
+        public Packages(DataRow dr)
+        {
+            this.Id = dr["id"].ToString();
+            this.Adress = dr["adress"].ToString();
+            this.PostalCode = dr["postalCode"].ToString();
+            this.District = dr["district"].ToString();
+            this.City = dr["city"].ToString();
+            this.Name = dr["name"].ToString();
+            this.IdentifierCode = int.Parse(dr["identifierCode"].ToString());
+
+        }
+
+
+    }
+}
---
+++ L4U-DAL-DATA/Utils/ConfigData.cs
@@ -0,0 +1,26 @@
+﻿namespace L4U_DAL_DATA.Utils
+{
+    public class ConfigData
+    {
+        public string Server { get; set; }
+        public string Database { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public string ConnString { get => $"Server = {Server}; Database = {Database}; User ID = {Username}; Password = {Password}; Trusted_Connection = False; Pooling = False;"; }
+
+        public ConfigData()
+        {
+            this.Server = @"l4userver.database.windows.net";
+            this.Database = "L4U-DB";
+            this.Username = "supergrupoadmin";
+            this.Password = "supergrupo+2022";
+        }
+
+        public string GetConnectionString() => ConnString;
+
+
+
+
+    }
+}
