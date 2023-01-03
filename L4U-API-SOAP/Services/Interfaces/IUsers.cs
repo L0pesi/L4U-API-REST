d40d1540c9ce4a7124ed20d71a8c2c5f76702182
Sérgio Lopes <sergiomaciel21@gmail.com>
2023-01-03 01:33:53 +0000

    Adicionei o serviço SOAP com: Add New User Lista todos os Users (com erros)

---
+++ L4U-API-SOAP/Services/Interfaces/IUsers.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using L4U_API_SOAP.SoapModels;
+
+
+namespace L4U_API_SOAP.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IUsers" in both code and config file together.
+    [ServiceContract]
+
+    public interface IUsers
+    {
+
+        /// <summary>
+        /// Lista todos os utilizadores
+        /// </summary>
+        /// <returns></returns>
+        [OperationContract]
+        List<User> GetAllUsers();
+
+
+
+        /// <summary>
+        /// Adiciona novo Utilizador
+        /// </summary>
+        /// <param name="User"></param>
+        /// <returns></returns>
+        [OperationContract]
+        bool AddNewUtilizador(User User);
+
+
+    }
+
+
+
+
+}
